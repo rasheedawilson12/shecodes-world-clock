55462c582fb1c2c5dd6c4874358aaf10b0f9f925
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-12-02 17:54:08 -0600

    customized css, convert to military time, more cities added

--- index.js
+++ index.js
@@ -7,22 +7,51 @@ function updateTime() {
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
     losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
-      "h:mm:ss [<small>] A[</small>]"
-    );
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss");
   }
 
-  // paris
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = paris.querySelector(".date");
-    let parisTimeElement = paris.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
+  // london
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = london.querySelector(".date");
+    let londonTimeElement = london.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
 
-    parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
-      "h:mm:ss [<small>] A[</small>]"
-    );
+    londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
+    londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+  }
+
+  //sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydney.querySelector(".date");
+    let sydneyTimeElement = sydney.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss");
+  }
+
+  // seoul
+  let seoulElement = document.querySelector("#seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoul.querySelector(".date");
+    let seoulTimeElement = seoul.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
+
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format("HH:mm:ss");
+  }
+
+  // nairobi
+  let nairobiElement = document.querySelector("#nairobi");
+  if (nairobiElement) {
+    let nairobiDateElement = nairobi.querySelector(".date");
+    let nairobiTimeElement = nairobi.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
+
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do, YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format("HH:mm:ss");
   }
 }
 
@@ -40,10 +69,9 @@ function updateCity(event) {
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
           </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format(" A")}</small></div>
+          <div class="time">${cityTime.format("HH:mm")}</div>
         </div>
+        <a href="/">&#8592; Return to Cities</a>
   `;
 }
 
